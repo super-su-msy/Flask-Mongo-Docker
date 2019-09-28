1ea882f8eef5a22cf9ce534a75eaa4bcb22b39f0
MK <karan.m@gmx.com>
2019-09-28 18:55:16 -0400

    Defining a function for validation of the data

--- api.py
+++ api.py
@@ -4,6 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+def checkdata(postedData, operation):
+    if (operation == "div"):
+        if "x" not in postedData or "y" not in postedData:
+            return 305
+        elif "x" == 0:
+            return 306
+        else:
+            return 200
+
 class firstApi(Resource):
      def post(self):
 
@@ -17,6 +27,24 @@ class firstApi(Resource):
             'succes': 'true'
         }
         return jsonify(response)
-   
 
+class secondApi(Resource):
+    def post(self):
+        
+        req = request.get_json()
+
+        statusCode = checkdata(req, "div")
+        if (statusCode == 305 or statusCode == 306):
+            return "you ugly BS"
+            
+        x = req["x"]
+        y = req["y"]
+
+        response = {
+            'result': x/y,
+            'succes': 'true'
+        }
+        return jsonify(response)
+            
 api.add_resource(firstApi, "/Resource")
+api.add_resource(secondApi, "/div")

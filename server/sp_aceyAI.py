1065505433b64af9aaa5ca833c449d907ffc134b
MK <karan.m@gmx.com>
2019-10-12 14:38:45 -0400

    Add spacy dependency to the docker instance

--- server/sp_aceyAI.py
+++ server/sp_aceyAI.py
@@ -1,6 +1,10 @@
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
-txt = nlp('This is Karan')
+# txt = nlp('This is Karan')
 
-print([(w.text, w.pos_) for w in txt])
+tokens = nlp("dog cat banana")
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
